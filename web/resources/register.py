d97371e70223e35bfa018dd057457e2fe33f8a90
Jovana Jovanovic <jjovana314@gmail.com>
2021-03-23 13:27:02 +0100

    insert_register_data and insert_bank_configuration funtions created

--- web/resources/register.py
+++ web/resources/register.py
@@ -18,22 +18,20 @@ class Register(Resource):
         data = request.get_json()
 
         is_valid, result = helper.validation(
-            config.users, schemas.register_schema, data, config.keys_register,
-            is_register=True
+            schemas.register_schema, data, is_register=True
         )
         if not is_valid:
             return jsonify(result)
-        else:
-            username, password = result
+        username_or_dict, password_or_false = helper.arguments_validation(result)
+        if isinstance(password_or_false, bool):
+            return jsonify(username_or_dict)
 
+        username, password = username_or_dict, password_or_false
         # crypt password for database
         # ! put this into smaller function
         if username == config.bank_name:
             return jsonify(
-                {
-                    "Message": "This username is taken.",
-                    "Code": config.INVALID_USERNAME
-                }
+                {"Message": "This username is taken.", "Code": config.INVALID_USERNAME}
             )
         hashed_pw = bcrypt.hashpw(password.encode("utf8"), bcrypt.gensalt())
 
@@ -42,26 +40,34 @@ class Register(Resource):
             config.bank_password.encode("utf8"), bcrypt.gensalt()
         )
         # ! put this into smaller function
-        config.users.insert(
-            {
-                "Username": username,
-                "Password": hashed_pw,
-                "Tokens": config.tokens_start,
-                "Balance": 0,
-                "Loan": 0,
-                "Sentance": ""
-            }
-        )
-        config.bank.insert(
-            {
-                "Username": config.bank_name,
-                "Password": password_bank_hashed,
-                "Balance": config.bank_balance
-            }
-        )
+        insert_register_data(username, hashed_pw)
+        
         return jsonify(
             {
                 "Code": config.OK,
                 "Message": "You succesfully signed up."
             }
         )
+
+
+def insert_register_data(username, hashed_pw):
+    config.users.insert(
+        {
+            "Username": username,
+            "Password": hashed_pw,
+            "Tokens": config.tokens_start,
+            "Balance": 0,
+            "Loan": 0,
+            "Sentance": ""
+        }
+    )
+
+
+def insert_bank_configuragion(password_bank_hashed):
+    config.bank.insert(
+        {
+            "Username": config.bank_name,
+            "Password": password_bank_hashed,
+            "Balance": config.bank_balance
+        }
+    )
