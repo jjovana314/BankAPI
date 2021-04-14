a93d7e51ad1bcdc12a2a2c182fad9a2c0d338546
Jovana Jovanovic <jjovana314@gmail.com>
2021-04-14 14:38:25 +0200

    validation caller added in post method

--- web/resources/take_loan.py
+++ web/resources/take_loan.py
@@ -18,44 +18,12 @@ class TakeLoan(Resource):
         """
         data = request.get_json()
 
-        try:
-            helper.validate_schema(schemas.loan_schema, data)
-        except exceptions.SchemaError as ex:
-            return jsonify(
-                {
-                    "message": "schema not match",
-                    "code": config.SCHEMA_NOT_MATCH
-                }
-            )
+        is_ok, result = helper.validation(schemas.loan_schema, data)
+        if not is_ok:
+            return result
         username = data["username"]
         password = data["password"]
         amount = data["amount"]
-        usr_exist = helper.username_exist(config.users, username)
-        if not usr_exist:
-            return jsonify(
-                {
-                    "message": "this useranem does not exist",
-                    "code": config.INVALID_USERNAME
-                }
-            )
-        password_valid = helper.verify_password(
-            config.users, password, username
-        )
-        if not password_valid:
-            return jsonify(
-                {
-                    "message": "password you entered is not valid",
-                    "code": config.INVALID_PASSWORD
-                }
-            )
-        tokens = helper.count_tokens(config.users, username)
-        if tokens <= 0:
-            return jsonify(
-                {
-                    "message": "you don't have enough tokens, please refill",
-                    "code": config.OUT_OF_TOKENS
-                }
-            )
 
         # ! repeatable code
         balance_usr = config.users.find(
@@ -76,10 +44,10 @@ class TakeLoan(Resource):
                 }
             )
         helper.update_balance(
-            config.users, username, amount, balance_usr, operator.add
+            username, amount, balance_usr, operator.add
         )
         helper.update_balance(
-            config.bank, config.bank_name, amount, balance_bank, operator.sub
+            config.bank_name, amount, balance_bank, operator.sub
         )
         helper.update_tokens(config.users, username, 1, operator.sub)
         return jsonify(
